4d734f8085f0e64fd1f44639f7b32759e9002f74
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-06-24 14:07:39 +0530

    Add files via upload

---
+++ Slicing_Part2.py
@@ -0,0 +1,40 @@
+'''Negative slicing'''
+x="welcome"
+# print(x[-10:])  #welcome
+# print(x[-7:])  #welcome
+# print(x[-6:])  #elcome
+# print(x[-5:])  #lcome
+# print(x[-4:])  #come
+# print(x[-3:])  #ome
+# print(x[-2:])  #me
+# print(x[-1:])  #e
+
+# print(x[-6:-3])  #elc
+# print(x[-7:-4])  #wel
+# print(x[-2:-4])  #Blank line
+# print(x[-1:-1])  #Blank line
+
+'''Slicing Double Colon'''
+# print(x[::])  #welcome
+# print(x[::-1])  #emoclew
+# print(x[::-2])  #eolw
+# print(x[::-4])  #el
+# print(x[::-5])  #ee
+# print(x[::-6])  #ew
+# print(x[::-7])  #e
+# print(x[::-8])  #e
+
+# print(x[::1])  #welcome
+# print(x[::2])  #wloe
+# print(x[::3])  #wce
+# print(x[::4])  #wo
+# print(x[::5])  #wm
+# print(x[::6])  #we
+# print(x[::7])  #w
+# print(x[::8])  #w
+
+# print(x[2::2])  #loe
+# print(x[3::3])  #ce
+# print(x[2::4])  #le
+# print(x[3:4:3])  #c
+# print(x[3:3:3])  #Blankline
